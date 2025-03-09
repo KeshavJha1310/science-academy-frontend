a17562b0b16a556b725a398a828e113bee5f6c35
Keshav Jha <124581153+KeshavJha1310@users.noreply.github.com>
2025-03-09 19:35:44 +0530

    Update content_option.js

--- src/MyComponents/content_option.js
+++ src/MyComponents/content_option.js
@@ -1,6 +1,6 @@
 const contactConfig = {
     YOUR_EMAIL: '  yabhay1521@gmail.com',
-    YOUR_FONE: '  +91-8149307762',
+    YOUR_FONE: '  +91-8149397762',
     description:
       'Our pleasure to talk with you !',
     // YOUR_SERVICE_ID: 'service_id',
